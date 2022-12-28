50b10e193727705027c19be29569959ca3a52926
Kristina <kristinapiljuk@gmail.com>
2022-12-29 00:20:21 +0300

    Курсач

--- java/kurs.js
+++ java/kurs.js
@@ -1,24 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', () => {
-
-    const siblings = el => [].slice.call(el.parentNode.children).filter(child => (child !== el)); // объявляем функцию, которая будет возвращать соседние элементы
-  
-    const main = document.querySelectorAll('.fat') // получаем все карточки
-  
-    main.forEach(fat => { // для каждой карточки
-      fat.addEventListener('mouseenter', () => { // при наведении на карточку
-        siblings(fat).forEach(el => { // для каждого соседнего элемента
-          el.classList.add('fat_sibling') // добавляем активный класс, который визуально ставит соседние карточки на задний план
-        })
-      })
-      fat.addEventListener('mouseleave', () => { // если убираем курсор с карточки
-        siblings(fat).forEach(el => { // для каждого соседнего элемента
-          el.classList.remove('fat_sibling') // удаляем активный класс
-        })
-      })
-    })
-  
-  })
-*/
 
   function onEntry(entry) {
     entry.forEach(change => {
